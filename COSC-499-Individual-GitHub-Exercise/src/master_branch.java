de622ce4be030c387cda1e1fda017cd1f764a640
qhu217 <97998169+qhu217@users.noreply.github.com>
2022-09-13 23:21:36 -0700

    Update master_branch.java

--- COSC-499-Individual-GitHub-Exercise/src/master_branch.java
+++ COSC-499-Individual-GitHub-Exercise/src/master_branch.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 
 public class master_branch {
 	
-	public static void sort_integers(int[] arr) {
-		//feature 1: if the user entered a list of integers, use this method
-		
-	}
 	
 	public static void main(String[] args) {
 		// main function: ask for user to enter a list of numbers or characters
