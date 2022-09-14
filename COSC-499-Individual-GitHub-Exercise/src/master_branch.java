af7ccd43017970d79cf16c50f3587616253030cf
qhu217 <97998169+qhu217@users.noreply.github.com>
2022-09-13 23:03:35 -0700

    main function
    ask for user to enter a list of numbers or characters

---
+++ COSC-499-Individual-GitHub-Exercise/src/master_branch.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class master_branch {
+
+	public static void main(String[] args) {
+		// master branch: ask for user to enter a list of numbers or characters
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter a list of numbers or characters: ");
+		
+		
+	}
+
+}
