a8256f74f6b6df205e0e21ba40aa592617077573
qhu217 <97998169+qhu217@users.noreply.github.com>
2022-09-13 23:03:53 -0700

    Update master_branch.java

--- COSC-499-Individual-GitHub-Exercise/src/master_branch.java
+++ COSC-499-Individual-GitHub-Exercise/src/master_branch.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class master_branch {
 
 	public static void main(String[] args) {
-		// master branch: ask for user to enter a list of numbers or characters
+		// main function: ask for user to enter a list of numbers or characters
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please enter a list of numbers or characters: ");
 		
