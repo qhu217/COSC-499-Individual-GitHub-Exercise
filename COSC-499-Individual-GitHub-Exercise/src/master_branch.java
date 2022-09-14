c5c24f7124bc7c90fa3448e1cd0b51dce0e14531
qhu217 <97998169+qhu217@users.noreply.github.com>
2022-09-13 23:21:19 -0700

    Update master_branch.java

--- COSC-499-Individual-GitHub-Exercise/src/master_branch.java
+++ COSC-499-Individual-GitHub-Exercise/src/master_branch.java
@@ -13,8 +13,12 @@ public class master_branch {
 		// main function: ask for user to enter a list of numbers or characters
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please enter the amount of integers or characters: ");
-		
-		
+		try {
+			int amount = sc.nextInt();
+		}
+		catch (Exception ex) {
+			System.out.println("You need to enter an integer.");
+		}
 		
 		
 	}
